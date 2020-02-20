2b03d037cc04818e7a17afdea414003273e741ca
remove <youksp@gmail.com>
2020-02-20 15:16:00 +0900

    SourceTree 한글 테스트

---
+++ 1_시작.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello, C++" << endl;
+}
+
+// 디자인 패턴
+// 1) 변화를 예측하고 이를 반영하여 설계
+// 2) 변경에 쉽게 적용할 수 있도록 단순하게 설계하고, 이후에 리팩토링을 통해 적용하는 방식
+//  => TDD
+//   - 테스트를 먼저 작성해서 실패한다.
+//   - 기능을 구현해서 테스트를 성공한다.
+//   - 리팩토링 한다.
+
+// Ctrl + F5: Compile + Run
+// Ctrl + F7: Compile
